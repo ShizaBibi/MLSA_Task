19466093657159672841144215fb5a0db2499096
ShizaBibi <149885428+ShizaBibi@users.noreply.github.com>
2024-05-06 06:52:30 -0700

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const siteProps = {
   email: "dilawaizpasha271@gmail.com",
   gitHub: "ShizaBibi",
   instagram: "",
-  linkedIn: "ShizaBibi",
+  linkedIn: "Shiza Bibi",
   medium: "",
   twitter: "",
   youTube: "",
